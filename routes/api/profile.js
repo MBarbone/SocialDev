450aa1c686eb5447cd3ee64c1caa9b950484a4cb
Mike <michael.barbone@gmail.com>
2019-09-06 14:25:32 -0400

    Add experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -144,7 +144,7 @@ router.get("/user/:user_id", async (req, res) => {
 });
 
 // Delete profile/user, & post
-// Public
+// Private
 router.delete("/", auth, async (req, res) => {
   try {
     await Profile.findOneAndRemove({ user: req.user.id });
@@ -155,5 +155,65 @@ router.delete("/", auth, async (req, res) => {
     res.status(500).send("server error");
   }
 });
+
+// PUT, add in exp and education. same collection so using PUT not POST
+// Private
+
+router.put(
+  "/experience",
+  [
+    auth,
+    [
+      check("title", "TItle is required.")
+        .not()
+        .isEmpty(),
+      check("company", "Company is required")
+        .not()
+        .isEmpty(),
+      check("from", "From date is required.")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
