5718791a266a0dd2f6bcc9ea7d4a2303ef045e56
Mike <michael.barbone@gmail.com>
2019-09-08 18:19:42 -0400

    React Router for login and register, onchanges, on submit, and start redux store

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,24 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/register";
 import Login from "./components/auth/login";
 import "./App.css";
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <section className="container">
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
           <Switch>
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
           </Switch>
-        </section>
-      </Fragment>
-    </Router>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormdata] = useState({
+    email: "",
+    password: ""
+  });
+
+  const { email, password } = formData;
+
+  const onFormChange = e => {
+    setFormdata({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="container">
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign In To Your Account
+      </p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => onFormChange(e)}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={e => onFormChange(e)}
+            required
+            autoComplete="off"
+          />
+        </div>
+
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Login;
