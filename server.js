420d4fe655265acfb6fe2e485e1e4f934e1a0005
Mike <michael.barbone@gmail.com>
2019-09-10 20:49:22 -0400

    modify first route

--- server.js
+++ server.js
@@ -17,10 +17,6 @@ app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
 app.use("/api/users", usersRoutes);
 app.use("/api/profile", profileRoutes);
 app.use("/api/auth", authRoutes);
