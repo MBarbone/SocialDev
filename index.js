90c10e366f12009c056da29cbdbf72587a839c62
Mike <michael.barbone@gmail.com>
2019-09-11 21:20:30 -0400

    deploy

--- index.js
+++ index.js
@@ -23,14 +23,11 @@ app.use("/api/profile", profileRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "../client/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}`);
