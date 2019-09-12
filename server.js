a76c6979c471b5c54721261ea7becdeda40352a8
Mike <michael.barbone@gmail.com>
2019-09-11 20:43:19 -0400

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
+const path = require("path");
 
 const connectDB = require("./db/db");
 const usersRoutes = require("./routes/api/users");
@@ -21,6 +22,15 @@ app.use("/api/users", usersRoutes);
 app.use("/api/profile", profileRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
+
+// serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}`);
